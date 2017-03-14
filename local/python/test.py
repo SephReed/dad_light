b30a9277370ed25e232f2e382ba3615c2c627dbe
SephReed <ShadyWillowCreek@gmail.com>
2017-03-14 03:55:53 +0000

    this is fine

--- local/python/test.py
+++ local/python/test.py
@@ -1,4 +1,35 @@
+#! /usr/bin/python
+#
 import subprocess
+import random
+from flup.server.fcgi import WSGIServer 
+import sys, urlparse
 
-e = 'echo "18=1" > /dev/pi-blaster'
-subprocess.call(e, shell=True)
+
+
+
+
+def setPWM(gpio_num, value) :
+	value *= value
+	print value
+	e = 'echo "'+str(gpio_num)+'='+str(value)+'" > /dev/pi-blaster'
+	subprocess.call(e, shell=True)
+
+
+def app(environ, start_response):
+	start_response("200 OK", [("Content-Type", "text/html")])
+	  
+	i = urlparse.parse_qs(environ["QUERY_STRING"])
+	yield ('&nbsp;')
+
+	if "q" in i:
+		yield (i['q'][0])
+
+	setPWM(18, random.uniform(0.0, 1.0))
+
+
+
+
+
+
+WSGIServer(app).run()
