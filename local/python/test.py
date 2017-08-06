7550ff5fb2d44faab0ee3e87a9d0a212ffb8ac99
SephReed <ShadyWillowCreek@gmail.com>
2017-08-06 19:06:18 +0000

    gonna do a big clear

--- local/python/test.py
+++ local/python/test.py
@@ -4,6 +4,8 @@ import subprocess
 import random
 from flup.server.fcgi import WSGIServer 
 import sys, urlparse
+import math
+from time import sleep
 
 
 
@@ -21,11 +23,38 @@ def app(environ, start_response):
 	  
 	i = urlparse.parse_qs(environ["QUERY_STRING"])
 	yield ('&nbsp;')
+	# yield ('&nbsp;' + "lalal" + str(environ["QUERY_STRING"]))
 
-	if "q" in i:
-		yield (i['q'][0])
 
-	setPWM(18, random.uniform(0.0, 1.0))
+	if "bright" in i:
+		brightness = float(i['bright'][0])
+		setPWM(18, brightness)
+
+	elif "mode" in i:
+		mode = str(i['mode'][0])
+		yield(mode)
+		if mode == "off":
+			setPWM(18, 0)
+
+		elif mode == "on":
+			setPWM(18, 1)
+
+		elif mode == "sawUp":
+			for i in range(1000):
+				setPWM(18, (i/20.0) % 1)
+				sleep(0.01)
+
+		elif mode == "sawDown":
+			for i in range(1000):
+				setPWM(18, 1 - ((i/20.0) % 1))
+				sleep(0.01)
+
+		elif mode == "wave":
+			for i in range(1000):
+				value = (math.sin(i/20.0)/2) + 0.5
+				setPWM(18, value)
+				sleep(0.01)
+	
 
 
 
